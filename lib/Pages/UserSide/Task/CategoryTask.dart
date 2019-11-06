0fd5433475ef7c9eda5551f26608ce1ba80387da
zzyang <zzyang@iastate.com>
2019-11-06 11:00:03 -0600

    Update user task page, with AppBar, TabView and fancy black-yellow color style

---
+++ lib/Pages/UserSide/Task/CategoryTask.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CategoryTask extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr));
+  }
+}
---
+++ lib/Pages/UserSide/Task/EasyMoneyTask.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EasyMoneyTask extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr));
+  }
+}
