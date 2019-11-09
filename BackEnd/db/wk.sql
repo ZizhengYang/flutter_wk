d7a7d752568a780f0ee218eb7dd4964a27d7c624
zzyang <zzyang@iastate.com>
2019-11-09 11:26:40 -0600

    Fix 2 pages

---
+++ BackEnd/db/wk.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS `UserManagement`;
+USE `UserManagement`;
+
+CREATE TABLE IF NOT EXISTS `user_account`(
+    `uId` INT UNSIGNED AUTO_INCREMENT KEY,                  -- user id
+    `uName` VARCHAR(40) NOT NULL UNIQUE,                    -- username
+    'uPlace' INT UNSIGNED,                                  -- living place: table-living_place
+    `uGender` CHAR(1) NOT NULL,                             -- gender
+    `uPassword` VARCHAR(40) NOT NULL,                       -- password
+    `uEMail` VARCHAR(50) NOT NULL UNIQUE,                   -- email
+    'uPhoneNum' VARCHAR(20) UNIQUE,                         -- phone number
+    `uRegDate` VARCHAR(10) NOT NULL                         -- registration date
+);
+
+CREATE TABLE IF NOT EXISTS `living_place` (
+    `id` INT UNSIGNED NOT NULL auto_increment,
+    `address` VARCHAR(20) COLLATE NOT NULL,
+    `road` VARCHAR(64) COLLATE NOT NULL,
+    `distinct` VARCHAR(64) COLLATE NOT NULL,
+    `city` VARCHAR(64) COLLATE utf8_unicode_ci NOT NULL,
+    `state` VARCHAR(64) COLLATE utf8_unicode_ci NOT NULL,
+    `nation` VARCHAR(64) COLLATE NOT NULL,
+    `zip` MEDIUMINT(8) UNSIGNED NOT NULL
+);
+
+CREATE DATABASE IF NOT EXISTS `OrganizationManagement`;
+USE `OrganizationManagement`;
+
+CREATE DATABASE IF NOT EXISTS `TaskManagement`;
+USE `TaskManagement`;
+
+CREATE DATABASE IF NOT EXISTS `SkillManagement`;
+USE `SkillManagement`;
+
+CREATE DATABASE IF NOT EXISTS `MaterialManagement`;
+USE `MaterialManagement`;
---
+++ lib/Pages/UserSide/CommunityPage.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import './Task/CategoryTask.dart';
+import './Task/EasyMoneyTask.dart';
+
+class CommunityPage extends StatelessWidget {
+
+  TextEditingController _searchBoxContronller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new DefaultTabController(
+        length: 2,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "waikuaiInfo",
+          home: new Scaffold(
+            /*
+         * Page main part
+         * 页面主要内容
+         */
+            appBar: new PreferredSize(
+                preferredSize: Size(1000,100),
+                child: new AppBar(
+                  backgroundColor: Colors.black,
+                  title: Container(
+                      alignment: Alignment.topCenter,
+                      child: new Column(
+                        children: <Widget>[
+                          new Text(
+                            "社区",
+                            style: new TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.yellow[700],
+                                decoration: TextDecoration.none
+                            ),
+                          ),
+                        ],
+                      )
+                  ),
+                  flexibleSpace: Container(
+                    child: new FlatButton(
+                      onPressed: () {},
+                      child: new TextField(
+                        maxLines: 1,
+                        controller: _searchBoxContronller,
+                        style: new TextStyle(
+                          fontSize: 16.0,
+                        ),
+                        decoration: new InputDecoration(
+                            hintText: "搜索外快",
+                            contentPadding: EdgeInsets.all(6),
+                            border: InputBorder.none
+                        ),
+                      ),
+                    ),
+                  ),
+                  bottom: new TabBar(
+                    indicatorWeight: 5,
+                    indicatorPadding: EdgeInsets.only(left: 15, top: 0, right: 15, bottom: 0),
+                    indicatorColor: Colors.yellow[700],
+                    labelColor: Colors.yellow[700],
+                    unselectedLabelColor: Colors.grey,
+                    tabs: <Widget>[
+                      new Tab(
+                        text: "干货",
+                      ),
+                      new Tab(
+                        text: "分享",
+                      ),
+                    ],
+                  ),
+                )
+            ),
+            body: new TabBarView(
+              children: <Widget>[
+                new CategoryTask(),
+                new EasyMoneyTask(),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+}
+
+//class UserTaskAppBar extends StatefulWidget implements PreferredSizeWidget {
+//
+//  final double height;
+//  final double elevation;
+//  final Widget text;
+//
+//  const UserTaskAppBar(
+//      {Key key,
+//        this.height: 46.0,
+//        this.elevation: 0.5,
+//        this.text: null,
+//      }
+//  ) : super(key: key);
+//
+//  @override
+//  State<StatefulWidget> createState() => new UserTaskAppBarState();
+//
+//  @override
+//  // TODO: implement preferredSize
+//  Size get preferredSize => Size.fromHeight(height);//这里设置控件（appBar）的高度
+//
+//}
+//
+//class UserTaskAppBarState extends State<UserTaskAppBar> {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//  }
+//
+//}
+//
+//class UserTaskBody extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//  }
+//
+//}
+
+//new Column(
+//children: <Widget>[
+///*
+//               * Upper part:
+//               * 上部分页面:
+//               * Page name 页面名称
+//               * Search box 搜索框
+//               * Top bar 上选框
+//               */
+//new Column(
+//children: <Widget>[
+//new Container(
+//color: Colors.black,
+//alignment: Alignment.topCenter,
+//padding: EdgeInsets.only(top: 30, left: 5, right: 5, bottom: 5),
+//child: new Text(
+//"外快信息",
+//style: new TextStyle(
+//fontSize: 30.0,
+//fontWeight: FontWeight.w900,
+//color: Colors.yellow[700],
+//decoration: TextDecoration.none
+//),
+//),
+//),
+//new Container(
+//color: Colors.black,
+//alignment: Alignment.topCenter,
+//padding: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
+//child: new Card(
+//child: new Row(
+//children: <Widget>[
+//new SizedBox(width: 5.0),
+//new Icon(Icons.search, color: Colors.grey),
+//Expanded(
+//child: new TextField(
+//maxLines: 1,
+//controller: _searchBoxContronller,
+//style: new TextStyle(
+//fontSize: 16.0,
+//),
+//decoration: new InputDecoration(
+//hintText: "搜索外快",
+//contentPadding: EdgeInsets.all(6),
+//border: InputBorder.none
+//),
+//),
+//),
+//new IconButton(icon: new Icon(Icons.cancel), onPressed: () {_searchBoxContronller.clear();}, color: Colors.grey)
+//],
+//),
+//),
+//),
+//new Container(
+//color: Colors.black,
+//child: new TabBar(
+//tabs: <Widget>[
+//new Tab(
+//text: "推荐",
+//icon: new Icon(Icons.thumb_up),
+//),
+//new Tab(
+//text: "热门",
+//icon: new Icon(Icons.whatshot),
+//),
+//new Tab(
+//text: "快赚",
+//icon: new Icon(Icons.fast_forward),
+//),
+//new Tab(
+//text: "分类",
+//icon: new Icon(Icons.grid_on),
+//)
+//],
+//),
+//),
+//],
+//),
+///*
+//             * Middle part:
+//             * 中部分页面:
+//             * Select view 筛选外块
+//             * Sort button 外快排序
+//             * List view 外快表单
+//             */
+//new Column(
+//children: <Widget>[
+//new TabBarView(
+//children: <Widget>[
+//new CategoryTask(),
+//new EasyMoneyTask(),
+//new HotTask(),
+//new RecommendationTask()
+//]
+//)
+//],
+//)
+//],
+//),
