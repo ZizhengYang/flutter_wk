717672df76f9d545a8bd92be5c8fb12beba88366
zzyang <zzyang@iastate.com>
2019-12-21 15:16:07 -0600

    Setting spring boot

--- wkBackEnd/xiaoqian/src/main/java/xiaoqian/XiaoqianApplication.java
+++ wkBackEnd/xiaoqian/src/main/java/xiaoqian/XiaoqianApplication.java
@@ -3,8 +3,8 @@ package xiaoqian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jdk.internal.jline.internal.Log;
-import lombok.extern.slf4j.Slf4j;
+//import jdk.internal.jline.internal.Log;
+//import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 public class XiaoqianApplication {
--- wkBackEnd/xiaoqian/src/main/java/xiaoqian/controller/UserController.java
+++ wkBackEnd/xiaoqian/src/main/java/xiaoqian/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.oracle.tools.packager.Log;
+//import com.oracle.tools.packager.Log;
 
 import xiaoqian.repository.UserRepository;
 
