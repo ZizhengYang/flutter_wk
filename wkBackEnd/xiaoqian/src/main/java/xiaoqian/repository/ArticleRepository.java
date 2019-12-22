10f0ce9eddb64ef0b07d1cf8dd97a2921090c456
ShenChen <schen41003@gmail.com>
2019-12-22 17:37:24 -0600

    acv

--- wkBackEnd/xiaoqian/src/main/java/xiaoqian/repository/ArticleRepository.java
+++ wkBackEnd/xiaoqian/src/main/java/xiaoqian/repository/ArticleRepository.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Repository;
 import lombok.Data;
 import xiaoqian.model.Article;
 import xiaoqian.model.Supervisor;
+import xiaoqian.model.User;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
 }
